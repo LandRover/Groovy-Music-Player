b369314d7b5c63835648d497464eb08a4229ddb0
Oleg <github.com@landroverz.com>
2015-06-14 16:19:42 +0300

    public API exposed.

--- src/javascript/bind/jquery.js
+++ src/javascript/bind/jquery.js
@@ -11,40 +11,23 @@ define([
         var args = Array.prototype.slice.call(arguments).slice(1), //Convert it to a real Array object.
             obj = null; 
         
-        console.log('BIND/JQUERY - INIT 501');
-        console.log(jQuery.gPlayerInstance);
-        console.log(gPlayer);
-        
         // bind instance if none found
         var create = (function() {
             // return if instance already exists
             if ('undefined' !== typeof(jQuery.gPlayerInstance)) return;
             
+            console.log('BIND/JQUERY - INIT 501');
+            console.log(jQuery.gPlayerInstance);
+            console.log(gPlayer);
+        
             var instance = new gPlayer(options || {})
                 .setEl(this)
                 .bootstrap();
-
+            
             // @todo Change position, instance should be stored in a local scope rather than in jquert scope to have multiple instances
             jQuery.gPlayerInstance = instance;
         })();
         
-        // object is passed only during the instance creation with options.. so anything else is a direct invoke.
-        var invoke = (function(method) {
-            if ('string' !== typeof(method)) return;
-            
-            console.log(['CALL', method, API]);
-            
-            //change condition.. not needed any more, run if method exists.
-            if ('string' === typeof(method)) {
-                console.log('BIND/JQUERY - METHOD FOUND, CALL IT 906');
-                obj = API.entry.apply(this, args);
-            }
-            
-            console.log('BIND/JQUERY - DONE 907');
-            console.log(obj);
-            console.log(API);
-            
-            return obj;
-        })(options);
+        return API;
     };
 });
