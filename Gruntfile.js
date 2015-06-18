8013b4682fa50e683600839d2bf56c8d4847216b
Oleg <github.com@landroverz.com>
2015-06-18 11:04:27 +0300

    added webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,6 @@
 module.exports = function (grunt) {
     /*
-    Install Grunt:
-    -------------------
-        npm install -g grunt-cli grunt-init
-        npm init (reads existing package.json file)
-    
-    Install Grunt Prerequisites:
+    Install Grunt and other prerequisites:
     ---------------------
         npm install
     
@@ -15,36 +10,29 @@ module.exports = function (grunt) {
         gem install compass
         gem install image_optim
     */
+    var path = require('path');
     
     // Print the execution time for the tasks
     require('time-grunt')(grunt);
-
-    grunt.initConfig({
+    
+    // loads npm
+    require('load-grunt-tasks')(grunt);
+    
+    require('load-grunt-config')(grunt, {
+        configPath: path.join(process.cwd(), '/build/tasks'),
         config: {
             pkg: grunt.file.readJSON('package.json'),
-
+            
             build: {
                 src: './src/javascript',
                 dest: 'dist',
                 banner: require('fs').readFileSync(__dirname + '/build/banner.txt', 'utf8')
             }
-        },
-    
-    build: {
-        all: {
-            dest: "dist/gplayer.js"
         }
-    },
-    watch: {
-        files: ['./src/**/*.js'],
-        tasks: ['build']
-    }
     });
-
-    require("load-grunt-tasks")(grunt);
-
+        
     // load grunt folder tasks.
-    grunt.loadTasks('build/tasks');
+    grunt.loadTasks('build');
 
     // Default task
     grunt.registerTask('default', [
