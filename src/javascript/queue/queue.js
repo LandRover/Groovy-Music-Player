775754a31ec78137eee2d729fec07cc4d0eec361
Oleg <github.com@landroverz.com>
2015-06-25 21:02:25 +0300

    set active on queue update added

--- src/javascript/queue/queue.js
+++ src/javascript/queue/queue.js
@@ -56,7 +56,7 @@ define([
             _.each(items, function(item) {
                 itemObj = new Item(item)
                     .render()
-                    .append('.'+ this.getDomParent());
+                    .append(this.getDomParent());
                 
                 this._notifications.fire(Events.QUEUE_ITEM_ADDED, itemObj);
             }, this);
@@ -71,7 +71,78 @@ define([
          *
          */
         updateList: function(item) {
-
+            console.log('LIST UPDATED');
+            
+            var size = this.getSize(),
+                itemPosition = item.getPosition();
+            
+            if (this.isEmpty()) {
+                this._notifications.fire(Events.QUEUE_EMPTY);
+            }
+            
+            //active song has been changed, set the next on active mode.
+            if (null === this.getActive()) {
+                //the active was the last on the playlist, select the new last.
+                if (itemPosition === size)
+                    itemPosition = size - 1;
+                
+                if (!this.isEmpty()) {
+                    this.setActive(itemPosition);
+                    //this._onPlay(this._getItemByIndex(itemPosition)); //@todo activate.
+                }
+            }
+        },
+        
+        
+        /**
+         * Get the active song from the queue and the next and previous songs as an option
+         *
+         * @param {Number} prevNext (-1 - previous item, 0 - current item (default value), 1 - next item)
+         * @return {Object} jQuery selector of the active object (or next, previous.. etc.)
+         */
+        getActive: function (prevNext) {
+            var item = (-1 === this.getDomParent().find('li.active').index()) ? null : this.getDomParent().find('li.active'),
+                direction = prevNext || 0,
+                ret = null,
+                index = null;
+            
+            if (null !== item) {
+                index = item.index() + direction;
+                
+                if (0 <= index)
+                    ret = this._getItemByIndex(index);
+            }
+            
+            return ret;
+        },
+        
+        
+        /**
+         * Sets the active queue item by IndexID.
+         *
+         * @param {Number} index - index of the item to be set active.
+         * @return {Object} jQuery selector of the new active object after the change
+         * 
+         * @todo: make better, remove the last active more efficently - keep track of the last played item. (might become an issue on longer lists)
+         */
+        setActive: function (index) {
+            var item = this._getItemByIndex(index);
+            
+            // verify if index exists.
+            if (null === item || (null !== this.getActive() && (
+                //prevent from re-setting the current active item again.
+                this.getActive().index() === item.index() &&
+                true === this.isPlaying()
+            ))) return null; //now playing similar to currently playing song.
+            
+            // Sets active flag on the index
+            this.getDomParent().find('li').removeClass('active');
+            item.addClass('active');
+        
+            // Fires the event notifying the item change.
+            this._notifications.fire(Events.QUEUE_ITEM_CHANGE_ACTIVE, item);
+            
+            return item;
         },
         
         
@@ -90,11 +161,19 @@ define([
         /**
          *
          */
+        isEmpty: function() {
+            return (0 >= this.getSize());
+        },
+        
+        
+        /**
+         *
+         */
         getDomParent: function() {
-            return this.getController()
+            return $('.' + this.getController()
                 .getModel()
                 .classes
-                .queue;
+                .queue);
         },
         
         
@@ -104,6 +183,18 @@ define([
         getController: function() {
             return gPlayer()
                 .getController();
+        },
+        
+        
+        /**
+         *
+         */
+        _getItemByIndex: function (index) {
+            var item = this.getDomParent()
+                .find('li:not(.cancel)')
+                .eq(index);
+        
+            return (0 > item.index()) ? null : item;
         }
     };
     
