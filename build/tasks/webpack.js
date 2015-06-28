f68e5897c369a12ed979877af106ec8f11e9b59d
Oleg <github.com@landroverz.com>
2015-06-28 07:29:15 +0300

    release mode fixed

--- build/tasks/webpack.js
+++ build/tasks/webpack.js
@@ -97,14 +97,14 @@ module.exports = function(grunt, options) {
             
             plugins: [
                 new webpack.DefinePlugin({
-                    __DEBUG__: true
-                })
+                    __DEBUG__: false
+                }),
                 
-                //new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-                //new webpack.optimize.DedupePlugin(),
-                //new webpack.optimize.UglifyJsPlugin({ sourceMap: false }),
-                //new webpack.optimize.OccurenceOrderPlugin()
-                //new webpack.optimize.AggressiveMergingPlugin()
+                new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+                new webpack.optimize.DedupePlugin(),
+                new webpack.optimize.UglifyJsPlugin({ sourceMap: false }),
+                new webpack.optimize.OccurenceOrderPlugin(),
+                new webpack.optimize.AggressiveMergingPlugin()
             ]
         }
     };
