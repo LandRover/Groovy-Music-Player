bf94d668c72b861a06e3ad0970a98d4f75a03b53
Oleg <github.com@landroverz.com>
2015-07-02 16:14:12 +0300

    comments added, rendering change slightly. Still dont like it

--- src/javascript/queue/view/item.js
+++ src/javascript/queue/view/item.js
@@ -6,7 +6,9 @@ define([
     "html/queue/item.html",
 ], function(BaseView, Events, States, Logger, itemHTML) {
     /**
+     * Queue Item view object.
      *
+     * Represents the view of the queue item, binds all clicks and ui listeners to this instance
      */
     var Item = function(item) {
         this._model = item;
@@ -14,13 +16,14 @@ define([
         this.init();
     };
     
+    
     Item.prototype = _.extend(new BaseView(), {
         _id: null,
         _model: null,
         
         
         /**
-         *
+         * Constructor method, called during construction
          */
         init: function() {
             Logger.debug('ITEM::INIT FIRED');
@@ -30,49 +33,52 @@ define([
         
         
         /**
-         *
+         * Rendering the output object
+         * 
+         * @return {Object} Item instance
          */
         render: function() {
-            this.output = this.bindActions($(_.template(itemHTML)(
+            this.output = $(_.template(itemHTML)(
                 this._model
-            )));
+            ));
+            
+            this.bindActions();
             
             return this;
         },
         
         
         /**
-         *
+         * Bind clicks to the output HTMLElement
          */
-        bindActions: function(html) {
-            var self = this;
+        bindActions: function() {
+            var self = this,
+                html = this.output;
             
             html.attr({
                 id: this._id
             });
             
             // @todo split to more methods.. this will become a mess very quickly
-            $(html).find('.play').on('click', function () {
+            html.find('.play').on('click', function () {
                 self.getNotifications().fire(Events.QUEUE_ITEM_CLICK_PLAY, self);
             });
             
-            $(html).find('.pause').on('click', function () {
+            html.find('.pause').on('click', function () {
                 self.getNotifications().fire(Events.QUEUE_ITEM_CLICK_PAUSE, self);
             });
             
-            $(html).find('.remove').on('click', function () {
+            html.find('.remove').on('click', function () {
                 self.cachePosition();
                 self.remove();
                 self.getNotifications().fire(Events.QUEUE_ITEM_REMOVED, self);
             });
             
-            $(html).find('strong').on('click', function () {
+            html.find('strong').on('click', function () {
                 self.getNotifications().fire(Events.QUEUE_ITEM_CLICK_ARTIST, self);
             });
             
-            $(html).data('object', this);
-            
-            return html;
+            html.data('object', this);
         }
     });
     
