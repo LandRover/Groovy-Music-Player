df479758fe0bf04f43857ccaf89d5d3e46349bff
Oleg <github.com@landroverz.com>
2015-07-02 11:44:23 +0300

    added comments on the devices

--- src/javascript/utils/device.js
+++ src/javascript/utils/device.js
@@ -1,12 +1,36 @@
 define(function() {
-    // is device ios.. etc.
+    /**
+     * Device detection util
+     *
+     * Helper for getting data regarding the current device such as os, browser touch ability etc.
+     * Most of the elements are pre-processed and are not functions and can be accessed directly
+     *
+     * Object is a singleton
+     */
     var Device = {
+        /**
+         * Is touch enabled on this device?
+         * 
+         * @return {Bool}
+         */
         isTouch: ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch,
         
+        /**
+         * OS detection. Currently supports iOS and Android
+         * 
+         * @return {Bool}
+         */
         os: {
             iOS: (/iP(hone|od|ad)/i).test(navigator.userAgent.toLowerCase()),
             android: (/android/i).test(navigator.userAgent.toLowerCase())
         },
+        
+        
+        /**
+         * Browser detection, for all the major browser. A quick way to know the context
+         * 
+         * @return {Bool}
+         */
         browser: {
             firefox: (/firefox/i).test(navigator.userAgent.toLowerCase()),
             chrome: (/chrome/i).test(navigator.userAgent.toLowerCase())  && (/google/i).test(navigator.vendor.toLowerCase()),
@@ -14,6 +38,13 @@ define(function() {
             opera: (/opera/i).test(navigator.userAgent.toLowerCase()),
         },
         
+        
+        /**
+         * Is mobile device? Based on OS.
+         * Encapsulates the decision
+         * 
+         * @return {Bool}
+         */
         mobile: function() {
             return this.os.iOS || this.os.android
         }
